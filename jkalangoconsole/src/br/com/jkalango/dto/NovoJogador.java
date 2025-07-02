97adedf8627fde3cf41817b55fad1e8e42d4808a
iara <iaraotako@gmail.com>
2025-07-02 09:45:45 -0300

    ATUALIZAÇÃO DO CÓDIGO

--- jkalangoconsole/src/br/com/jkalango/dto/NovoJogador.java
+++ jkalangoconsole/src/br/com/jkalango/dto/NovoJogador.java
@@ -3,9 +3,8 @@ package br.com.jkalango.dto;
 import javax.swing.JOptionPane;
 
 public class NovoJogador {
-
     private String nome;
-
+    
     public NovoJogador(String nome) {
         this.nome = nome;
     }
@@ -18,11 +17,13 @@ public class NovoJogador {
         this.nome = nome;
     }
 
-    public void verificarNome() {
-        if (nome == null || !nome.contains("java")) {
-            JOptionPane.showMessageDialog(null, "E necessario conter o nome JAVA vinculado ao seu nome'.");
-        } else {
-            JOptionPane.showMessageDialog(null, "Bem vindo, SuperJava");
+    public void verificarJava(){
+        if (getNome().toLowerCase().contains("java")) {
+            JOptionPane.showMessageDialog(null,"Jogador cadastrado com sucesso!");
+            System.exit(0);
+        }else{
+            JOptionPane.showMessageDialog(null, "Jogador Não pode ser Cadastrado, precisa conter 'java' no nome.", "ERRO", JOptionPane.ERROR_MESSAGE);
         }
     }
+    
 }
--- jkalangoconsole/src/br/com/jkalango/view/JCadastroJogador.java
+++ jkalangoconsole/src/br/com/jkalango/view/JCadastroJogador.java
@@ -1,44 +1,56 @@
 package br.com.jkalango.view;
-
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+
+import br.com.jkalango.dto.NovoJogador;
+
 import javax.swing.JButton;
 
-public class JCadastroJogador extends JFrame {
-    public JCadastroJogador() {
-        setTitle("Faça parte do JKalango!");
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setSize(400, 450);
-        setLocationRelativeTo(null);
-        setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
-
-        JLabel lblNome = new JLabel("Nome:");
-        JTextField txtNome = new JTextField(20);
-
-        add(lblNome);
-        add(txtNome);
-
-        JButton btnCadastrar = new JButton("Cadastrar");
-
-        btnCadastrar.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String nome = txtNome.getText().trim();
-
-                if (nome.isEmpty()) {
-                    JOptionPane.showMessageDialog(null, "Posso favor preencher espaco em branco!", "Erro", JOptionPane.ERROR_MESSAGE);
-                } else {
-                    JOptionPane.showMessageDialog(null, "Jogador cadastrado com sucesso, Parabens por fazer o cadastro no JKalangoAPP!");
-                }
-            }
-        });
-
-        add(btnCadastrar);
-        setVisible(true);
+//extends é herança
+public class JCadastroJogador extends JFrame{
+    //Construtor inicializa os componentes do formulário
+    public JCadastroJogador(){
+       //Título da Janela 
+       setTitle("Faça parte do JKalango!");
+       //Garante que a aplicação não seja finalizada
+       setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
+       //tamanho
+       setSize(400,450);
+       //posição ao centro
+       setLocationRelativeTo(null);
+       setLayout(new FlowLayout(FlowLayout.CENTER,10,10));
+       JLabel lblNome = new JLabel("Nome:");
+       JTextField txtNome = new JTextField(20);
+       //'JPasswordField
+       add(lblNome);
+       add(txtNome);
+       JButton btnCadastrar = new JButton("Cadastrar");
+       btnCadastrar.addActionListener(new ActionListener(){
+       
+       String nome = txtNome.getText();  
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            NovoJogador jogador = new NovoJogador(txtNome.getText());
+            jogador.verificarJava();
+            // TODO Auto-generated method stub
+            /*if (txtNome.getText().trim().isEmpty()) {
+                JOptionPane.showMessageDialog(null, "O Campo Não Pode ser Vazio ou Nulo","Erro", JOptionPane.ERROR_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null,"Jogador cadastrado com sucesso!");
+                dispose();
+            } */       
+        }
+        
+       });
+       setVisible(true);
+       add(btnCadastrar);
     }
+    
 }
